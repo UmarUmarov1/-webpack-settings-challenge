cd4323178c34fe111dc286e8220ce93d8782a001
UmarUmarov1 <116254464+UmarUmarov1@users.noreply.github.com>
2022-12-02 15:52:00 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+document.addEventListener('DOMContentLoaded', async () => {
+  const response = await fetch("https://api.chucknorris.io/jokes/random");
+  const json = await response.json();
+
+  const root = document.getElementById('root');
+  const divWithJoke = document.createElement('div');
+
+  divWithJoke.append(json.value);
+
+  root.append(divWithJoke);
+})
+
